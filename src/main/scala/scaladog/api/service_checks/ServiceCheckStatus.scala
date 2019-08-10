16ff0b01eafcaf0c73d45c33e23c5d3cb05e7d4f
Takumi Kadowaki <tkadowaki.dev.blackey@gmail.com>
2019-08-10 16:21:16 +0900

    Add UPickleEnum for refactoring (#33)

--- src/main/scala/scaladog/api/service_checks/ServiceCheckStatus.scala
+++ src/main/scala/scaladog/api/service_checks/ServiceCheckStatus.scala
@@ -1,20 +1,17 @@
 package scaladog.api.service_checks
 
-import enumeratum.values.{IntEnum, IntEnumEntry}
-import scaladog.api.DDPickle
+import enumeratum.values.IntEnumEntry
+import scaladog.api.IntUPickleEnum
 
 import scala.collection.immutable.IndexedSeq
 
 sealed abstract class ServiceCheckStatus(val value: Int) extends IntEnumEntry
 
-object ServiceCheckStatus extends IntEnum[ServiceCheckStatus] {
+object ServiceCheckStatus extends IntUPickleEnum[ServiceCheckStatus] {
   case object OK       extends ServiceCheckStatus(0)
   case object Warning  extends ServiceCheckStatus(1)
   case object Critical extends ServiceCheckStatus(2)
   case object Unknown  extends ServiceCheckStatus(3)
 
   val values: IndexedSeq[ServiceCheckStatus] = findValues
-
-  implicit val writer: DDPickle.Writer[ServiceCheckStatus] =
-    DDPickle.writer[Int].comap(_.value)
 }
