82ff045f9e7652950795fc4bc8c00e2e6b375bff
NomadBlacky <tkadowaki.dev.blackey@gmail.com>
2019-11-06 23:49:53 +0900

    Provide default parameters by APIClientFactory#apply

--- src/main/scala/scaladog/api/APIClientFactory.scala
+++ src/main/scala/scaladog/api/APIClientFactory.scala
@@ -3,11 +3,9 @@ package scaladog.api
 import scaladog.ReadEnv
 
 trait APIClientFactory[T <: APIClient] extends ReadEnv {
-  def apply(): T = apply(
-    apiKey = readEnv("DATADOG_API_KEY"),
-    appKey = readEnv("DATADOG_APP_KEY"),
-    site = readEnvSite()
-  )
-
-  def apply(apiKey: String, appKey: String, site: DatadogSite): T
+  def apply(
+      apiKey: String = readEnv("DATADOG_API_KEY"),
+      appKey: String = readEnv("DATADOG_APP_KEY"),
+      site: DatadogSite = readEnvSite()
+  ): T
 }
